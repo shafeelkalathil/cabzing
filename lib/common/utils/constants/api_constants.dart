078918feefeb4727494137087439819e3b753b7c
Shafeel kalathil <mhd.shfeelkalathil@gmail.com>
2024-08-10 09:39:17 +0530

    initial commit

--- lib/common/utils/constants/api_constants.dart
+++ lib/common/utils/constants/api_constants.dart
@@ -3,6 +3,7 @@ class ApiConstants{
   static String loginBaseUrl = "https://api.accounts.vikncodes.com/api/v1/";
   ///profile
   static String baseUrl = "https://www.api.viknbooks.com/api/v10/";
+
   static String users = "users/";
   static String usersView = "user-view/";
   static String sales = "sales/";
